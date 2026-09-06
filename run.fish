65a7aada2d4483dcc423a5ffe0112bb2e15196ae
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2026-09-06 23:37:36 +0500

    fix: add root GNUmakefile (make run was pointing at nothing)

--- run.fish
+++ run.fish
@@ -1,2 +1,10 @@
 #!/usr/bin/env fish
-make run QEMUFLAGS="-m 2G -device piix3-ide,id=ide -drive id=disk,file=fs.img,format=raw,if=none -device ide-hd,drive=disk,bus=ide.0"
+make run QEMUFLAGS="-m 2G \
+  -device piix3-ide,id=ide \
+  -drive id=disk,file=fs.img,format=raw,if=none \
+  -device ide-hd,drive=disk,bus=ide.0 \
+  -device rtl8139,netdev=n0 \
+  -netdev user,id=n0 \
+  -object filter-dump,id=f0,netdev=n0,file=dump.pcap \
+  -audiodev pipewire,id=snd \
+  -machine pcspk-audiodev=snd"
